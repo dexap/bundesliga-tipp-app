88c258a33c85c89a9b88d2804f6446a2069e4548
Benjamin Lassmann <benjamin.lassmann@student.htw-berlin.de>
2024-08-26 10:51:53 +0200

    init

---
+++ auth-service/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	r := echo.New()
+
+	r.Use(middleware.Logger())
+	r.Use(middleware.Recover())
+
+	r.GET("/", func(c echo.Context) error {
+		return c.String(200, "auth-service says: Hello, World!")
+	})
+
+	r.Logger.Fatal(r.Start(":3000"))
+
+}
