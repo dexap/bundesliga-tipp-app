75358294e580246085a13eb8117338ffa8884f6c
Benjamin Lassmann <benjamin.lassmann@student.htw-berlin.de>
2024-08-26 11:51:16 +0200

    basic templ files

---
+++ frontend-service/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/dexap/bundesliga-tipp-app/frontend-service/templates"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		return templates.Home().Render(c.Response().Writer, c.Request())
+	})
+
+	e.Logger.Fatal(e.Start(":3000"))
+}
